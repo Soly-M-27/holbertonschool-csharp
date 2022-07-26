7196eb291ff1d9f0732eb0ada732d7947ea95e82
Soly-M-27 <solymar227@icloud.com>
2022-07-25 21:17:08 -0400

    Retest Alligment once more

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -4,11 +4,11 @@ using System.Linq;
 
 namespace writeline
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			Console.WriteLine("\"Programming is like building a multilingual puzzle\");
-		}
-	}
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
+    }
 }
