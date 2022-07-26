1c30a12e6f06d2dbb0a5827fdcac460548f534ea
Soly-M-27 <solymar227@icloud.com>
2022-07-26 16:32:19 -0400

    Fixed syntax on printiny 99

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -7,8 +7,7 @@ class Program
         for (int x = 0; x <= 99; x++)
         {
             Console.Write(string.Format("{0:00}, ", x));
-
         }
-        Console.Write("99/n");
+        Console.Write("99\n");
     }
 }
