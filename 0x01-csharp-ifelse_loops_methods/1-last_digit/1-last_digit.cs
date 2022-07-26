43d0d4273e0cdbb77ca6b8a9a91203b8ee2ec0a6
Soly-M-27 <solymar227@icloud.com>
2022-07-26 15:30:31 -0400

    Check of last digit of random num is positive, negative or equal to zero

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -9,7 +9,7 @@ class Program
         mod_num = number % 10;
         if (mod_num > 5)
         {
-            Console.WriteLine("The last digit of {0:D} is {1:D} and is less than 6 and not 0", number, mod_num);
+            Console.WriteLine("The last digit of {0:D} is {1:D} and is greater than 5", number, mod_num);
         }
         else if (mod_num == 0)
         {
@@ -17,7 +17,7 @@ class Program
         }
         else
         {
-            Console.WriteLine("The last digit of {0:D} is {1:D} and is greater than 5", number, mod_num);
+            Console.WriteLine("The last digit of {0:D} is {1:D} and is less than 6 and not 0", number, mod_num);
         }
     }
 }
