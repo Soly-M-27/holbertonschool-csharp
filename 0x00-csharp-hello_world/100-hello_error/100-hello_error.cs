b747c2edfd1d214cec799ea42c4f8f39f449c165
Soly-M-27 <solymar227@icloud.com>
2022-07-25 23:44:20 -0400

    Print to stderr and exit with status code 1

---
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace STDERR
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.Error.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+            return (1);
+        }
+    }
+}
