5af8ceb59c08e0a36506fbbad16e5589a6e1adcd
Soly-M-27 <solymar227@icloud.com>
2022-07-25 22:53:00 -0400

    Spliciing in C#

--- 0x00-csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
+++ 0x00-csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -5,9 +5,11 @@ class Program
         static void Main(string[] args)
         {
 		string word = "Holberton";
-		// YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-		Console.WriteLine("First 3 letters: {0}", word_first_3.Substring(0,3));
-		Console.WriteLine("Last 2 letters: {0}", word_last_2.Substring(8,9));
-		Console.WriteLine("Middle word: {0}", middle_word.Substring(1,8));
+		string word_first_3 = word.Substring(0,3);
+		string word_last_2 = word.Substring(8,2);
+		string middle_word = word.Substring(1,7);
+		Console.WriteLine("First 3 letters: {0}", word_first_3);
+		Console.WriteLine("Last 2 letters: {0}", word_last_2);
+		Console.WriteLine("Middle word: {0}", middle_word);
         }
 }
