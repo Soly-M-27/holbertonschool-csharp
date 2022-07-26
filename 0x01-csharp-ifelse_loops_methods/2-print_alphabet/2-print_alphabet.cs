b2afc0a2a9eea8205979867b574779e3087e37b8
Soly-M-27 <solymar227@icloud.com>
2022-07-26 15:48:32 -0400

    Print the alphabet in lowe case letter with no new line

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char alphabet = 'a'; alphabet <= 'z'; alphabet++)
+        {
+            Console.Write(alphabet);
+        }   
+    }
+}
