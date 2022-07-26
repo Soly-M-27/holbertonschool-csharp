cb180acb0084cd010dd14cc6171153035b2d39f4
Soly-M-27 <solymar227@icloud.com>
2022-07-26 15:28:28 -0400

    Check of last digit of random num is positive, negative or equal to zero

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        mod_num = number % 10;
+        if (mod_num > 5)
+        {
+            Console.WriteLine("The last digit of {0:D} is {1:D} and is less than 6 and not 0", number, mod_num);
+        }
+        else if (mod_num == 0)
+        {
+            Console.WriteLine("The last digit of {0:D} is {1:D} and is 0", number, mod_num);
+        }
+        else
+        {
+            Console.WriteLine("The last digit of {0:D} is {1:D} and is greater than 5", number, mod_num);
+        }
+    }
+}
