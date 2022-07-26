8a0200bf7e31606aa6fc8feaf0645b908dd00046
Soly-M-27 <solymar227@icloud.com>
2022-07-26 17:00:20 -0400

    Print all possible combs of 2 digits

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int x = 0; x <= 7; x++)
+        {
+            for (int y = 0; y <= 9; y++)
+            {
+                if (x < y && x != y)
+                {
+                    Console.Write("{0}{1}, ", x, y);
+                }
+            }
+        }
+        Console.WriteLine("89");
+    }
+}
