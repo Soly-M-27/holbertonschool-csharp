7176df19764af7763528dd579e2d95ab7c627270
Soly-M-27 <solymar227@icloud.com>
2022-07-25 22:02:30 -0400

    Deleted new line

--- 0x00-csharp-hello_world/8-print_variables/8-print_variables.cs
+++ 0x00-csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -7,6 +7,6 @@ class Program
         string str1 = "Street";
         string str2 = "Mission";
         int int1 = 972;
-        Console.WriteLine("{0}{2}{1}\n", int1, str1, str2);
+        Console.WriteLine("{0}{2}{1}", int1, str1, str2);
     }
 }
