25731c96fc0b238f438f6b26ed53710231a204da
Soly-M-27 <solymar227@icloud.com>
2022-07-26 16:29:06 -0400

    Write a program that prints numbers from 0 to 99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int x = 0; x <= 99; x++)
+        {
+            Console.Write(string.Format("{0:00}, ", x));
+        }
+        Console.Write("99/n");
+    }
+}
