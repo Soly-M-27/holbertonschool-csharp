a2ff6a38b462f0b03eea71319ab7f694e676d3ca
Soly-M-27 <solymar227@icloud.com>
2022-07-26 17:27:08 -0400

    Return the absolute value of the last digit of a number

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int last_dig = number % 10;
+        Console.Write("{0}", Math.Abs(last_dig));
+        return Math.Abs(last_dig);
+    }
+}
