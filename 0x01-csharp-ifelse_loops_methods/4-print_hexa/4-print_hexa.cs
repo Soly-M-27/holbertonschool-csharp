0d37ba84c7e643972b031912438fd7a0b2a104d4
Soly-M-27 <solymar227@icloud.com>
2022-07-26 16:10:05 -0400

    Loop from 0 to 98 and print said number as decimals and hexadecimals

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,19 @@
+﻿/** Write a program that prints all numbers from 0 to 98 
+ * in decimal and in hexadecimal (as in the following example)
+ *
+ *  -You can only use Console.Write once
+ *  -You can only use one loop in your code
+ **/
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int x = 0; x <= 98; x++)
+        {
+            Console.Write("{0:D} = 0x{0:X}\n", x);
+        }
+    }   
+}
