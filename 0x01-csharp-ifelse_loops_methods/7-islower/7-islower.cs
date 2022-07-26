ca4924bb171599191354eae6ed34b872cba649c2
Soly-M-27 <solymar227@icloud.com>
2022-07-26 17:16:05 -0400

    Check if letter is lowercase or not with bool

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        for (char alphabet = 'a'; alphabet <= 'z'; alphabet++)
+        {
+            if (alphabet == c)
+            {
+                return True;
+            }
+        }
+        return False;
+    }
+}
