3c804e18afb4bdb5abb0e230448dc13b45aa452f
Soly-M-27 <solymar227@icloud.com>
2022-07-26 16:36:50 -0400

    Changed variable value to 98 form 99 in for loop

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        for (int xer = 0; xer <= 99; xer++)
+        for (int x = 0; x <= 98; x++)
         {
-            Console.Write(string.Format("{0:00}, ", xer));
+            Console.Write(string.Format("{0:00}, ", x));
         }
         Console.Write("99\n");
     }
