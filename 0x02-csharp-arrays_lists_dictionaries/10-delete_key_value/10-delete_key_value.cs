9bff56695fd0da57d918409697a6aa5829d63edf
Soly-M-27 <solymar227@icloud.com>
2022-07-27 22:45:49 -0400

    Added return at end of method

--- 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
+++ 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -6,7 +6,6 @@ class Dictionary
     public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
     {
         myDict.Remove(key);
+        return myDict;
     }
-
-    return myDict;
 }
