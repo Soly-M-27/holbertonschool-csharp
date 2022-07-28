a989d3398084cbaad69cbf3f46c85087a267aea8
Soly-M-27 <solymar227@icloud.com>
2022-07-27 21:21:18 -0400

    Method that creates and prints a list of integers of a given size

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        List<int> list = new List<int>();
+        
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+        
+        for (int x = 0; x < size; x++)
+        {
+            list.Add(x);
+            Console.Write(list[x]);
+            if (x != size - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+    
+    Console.WriteLine();
+    return list;
+    }
+}
