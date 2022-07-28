31a47c494072ff3bd225408b3324736318a23100
Soly-M-27 <solymar227@icloud.com>
2022-07-27 21:33:52 -0400

    Remove key from dict

---
+++ 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Dictionary
+{
+    public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
+    {
+        myDict.Remove(key);
+    }
+
+    return myDict;
+}
