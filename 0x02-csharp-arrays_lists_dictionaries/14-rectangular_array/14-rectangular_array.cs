5d5fecd0287b59bb3c0a5835b28fd47ce6827866
Soly-M-27 <solymar227@icloud.com>
2022-07-27 22:54:08 -0400

    program that creates and prints a 5 by 5 two-dimensional array and initialize index [2,2] to 1 and all other indices to 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i, j;
+        int[,] arrays = new int[,] 
+        { 
+            {0, 0, 0, 0, 0}, 
+            {0, 0, 0, 0, 0}, 
+            {0, 0, 1, 0, 0}, 
+            {0, 0, 0, 0, 0}, 
+            {0, 0, 0, 0, 0}
+        };
+
+        for (i = 0; i < arrays.GetLength(0); i++)
+        {
+            for (j = 0; j < arrays.GetLength(1); j++)
+            {
+                Console.Write(arrays[i, j]);
+                if (j != arrays.GetLength(1) - 1)
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
