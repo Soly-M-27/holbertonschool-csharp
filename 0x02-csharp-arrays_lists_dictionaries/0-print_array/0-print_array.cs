c76c6d4ddf7db3c084088196cd03d1328ae1c7e1
Soly-M-27 <solymar227@icloud.com>
2022-07-27 21:08:19 -0400

    method that creates and prints an array of integers of a given size

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+        int[] array = new int[0];
+        int x = 0;
+
+        if (size > 0)
+        {
+            array = new int[size];
+            for (x = 0; x < size; x++)
+            {
+                array[x] = x;
+                Console.Write(array[x]);
+                if (array[x] != size - 1)
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+        Console.WriteLine();
+        return array;
+    }
+}
