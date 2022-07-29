8b58518ae418c756af3acb8191262eb45ef4b8a7
Soly-M-27 <solymar227@icloud.com>
2022-07-28 21:15:24 -0400

    Display specified items of the Stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aStack.Count}");
+        
+        if (aStack.Count > 0)
+        {
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        }
+        else
+        {
+            Console.WriteLine("Stack is empty");
+        }
+
+        Console.WriteLine($"Stack contains \"{search}\": {(aStack.Contains(search) ? "True" : "False")}");
+
+        if (aStack.Contains(search))
+        {
+            string result;
+            do
+            {
+                result = aStack.Pop();
+            } while (result != search);
+        }
+
+        aStack.Push(newItem);
+
+        return (aStack);
+    }
+}
