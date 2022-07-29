d9861b2fe38f59fa06512b4a7761760f1c2e580c
Soly-M-27 <solymar227@icloud.com>
2022-07-28 21:37:26 -0400

    method that deletes the head node of a LinkedList and returns that node’s data

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count == 0)
+        {
+            return (0);
+        }
+        
+        LinkedListNode<int> num = myLList.First;
+        
+        if (num == null)
+        {
+            return (0);
+        }
+        myLList.RemoveFirst();
+        return (num.Value);
+    }
+}
