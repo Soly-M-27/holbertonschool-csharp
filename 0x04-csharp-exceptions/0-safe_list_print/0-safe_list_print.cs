e7aef249bb7818826f5e64adc43b87c4eda0291a
Soly-M-27 <solymar227@icloud.com>
2022-08-08 19:31:12 -0400

    print list safely with try catch to check if index is out of range

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{    
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int count = 0;
+
+        try
+        {
+            for (int x = 0; x < n; x++)
+            {
+                Console.WriteLine("{0:D}", myList[x]);
+                count++;
+            }
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            n = myList.Count;
+            return (n);
+        }
+        return (count);
+    }
+}
