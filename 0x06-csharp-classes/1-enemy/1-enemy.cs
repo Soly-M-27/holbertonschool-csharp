67c400281565915bb73eb0df59897aeea9f6a227
Soly-M-27 <solymar227@icloud.com>
2022-08-09 18:47:32 -0400

    Created public field and constructor for previous Enemies.Zombie Namespace/Class

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Public class within Enemies namespace
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// public field health set without declaration 
+        /// </summary>
+        public int? health;
+
+        /// <summary>
+        /// Public Constructor for Zombie class 
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
