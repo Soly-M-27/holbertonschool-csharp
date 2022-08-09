d2d0349ce9dbcfb77e044f64d953b03929fcb9ad
Soly-M-27 <solymar227@icloud.com>
2022-08-09 19:40:36 -0400

    Based on previous enemy program, added public constructor GetHealth to print private fields value

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Public class within Enemies namespace
+    /// </summary>
+    public class Zombie
+    {
+        /// private field health set without declaration 
+        private int _health;
+
+        /// <summary>
+        /// Public Constructor for Zombie class 
+        /// </summary>
+        public Zombie()
+        {
+            _health = 0;
+        }
+        /// <summary>
+        /// New Public Constructor for Zombie where
+        /// value must be greater than or equal to 0.
+        /// If value is less than 0, throw an ArgumentException
+        /// with the message "Health must be greater or equal to 0   
+        /// </summary>
+        /// <param name="value"></param>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+            {
+                this._health = value;
+            }
+            else
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+        /// <summary>
+        /// Returns the value of health
+        /// of the Zombie object 
+        /// </summary>
+        /// <returns>private field _health</returns>
+        public int GetHealth()
+        {
+            return _health;
+        }
+    }
+}
