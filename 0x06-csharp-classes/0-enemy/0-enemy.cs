fbd8cb17f6a87a7173f7fc59d9de33aff53c93e6
Soly-M-27 <solymar227@icloud.com>
2022-08-09 17:46:38 -0400

    ADD ALL

---
+++ 0x06-csharp-classes/0-enemy/0-enemy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        public string name { get; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
