425f67d04b9163579dbc050ea36eb8deff222143
Soly-M-27 <solymar227@icloud.com>
2022-08-09 19:16:14 -0400

    Based on 1-enemy model, creater another public constructor to handle if a value entered is greater or equal to 0 and assign said value to health if positive

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Public class within Enemies namespace
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// public field health set without declaration 
+        /// </summary>
+        public int? health;
+
+        /// <summary>
+        /// Public Constructor for Zombie class 
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary>
+        /// New Public Constructor for Zombie where
+        /// value must be greater than or equal to 0.
+        /// If value is less than 0, throw an ArgumentException
+        /// with the message "Health must be greater or equal to 0   
+        /// </summary>
+        /// <param name="value"></param>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+            {
+                this.health = value;
+            }
+            else
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+        
+    }
+}
