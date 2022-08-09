d49dd7ee710e152ea8ef0c1c95cf5cb0a51d1022
Soly-M-27 <solymar227@icloud.com>
2022-08-08 20:18:00 -0400

    Returned new list with results of the division of elements between two different lists with different list lengths

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> new_list = new List<int>();
+        int div = 0;
+
+        for (int x = 0; x < listLength; x++)
+        {
+            try
+            {
+                div = list1[x] / list2[x];
+                new_list.Add(div);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of Range");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannit divide by zero");
+                new_list.Add(0);
+            }
+        }
+        return (new_list);
+    }
+}
