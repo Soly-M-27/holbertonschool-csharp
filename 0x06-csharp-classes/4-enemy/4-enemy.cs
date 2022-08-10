1d6b593f90911341b78a1a98594f03911829b98b
Soly-M-27 <solymar227@icloud.com>
2022-08-09 20:01:31 -0400

    Added names to Zombie Class

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Public class within Enemies namespace
+    /// </summary>
+    public class Zombie
+    {
+        /// private field health set without declaration 
+        private int _health;
+        /// private field name
+        private string? _name = "(No name)";
+        /// <summary>
+        /// Public Property Name
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Public Constructor for Zombie class 
+        /// </summary>
+        public Zombie()
+        {
+            _health = 0;
+        }
+        /// <summary>
+        /// New Public Constructor for Zombie where
+        /// value must be greater than or equal to 0.
+        /// If value is less than 0, throw an ArgumentException
+        /// with the message "Health must be greater or equal to 0   
+        /// </summary>
+        /// <param name="value"></param>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+            {
+                this._health = value;
+                this._name = Name;
+            }
+            else
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+        /// <summary>
+        /// Returns the value of health
+        /// of the Zombie object 
+        /// </summary>
+        /// <returns>private field _health</returns>
+        public int GetHealth()
+        {
+            return _health;
+        }
+    }
+}
