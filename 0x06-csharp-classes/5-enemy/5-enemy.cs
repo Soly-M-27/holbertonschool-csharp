e3b3d649ec1adf030db030bae5432317476859f5
Soly-M-27 <solymar227@icloud.com>
2022-08-09 20:24:02 -0400

    Print out Zombie objetc's attributes using public override string ToString method

--- 0x06-csharp-classes/5-enemy/5-enemy.cs
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Public class within Enemies namespace
+    /// </summary>
+    public class Zombie
+    {
+        /// private field health 
+        private int _health;
+        /// private field _name
+        private string _name = "(No name)";
+
+        /// <summary>
+        /// Public Property Name
+        /// </summary>
+        public string Name { get => _name; set => _name = value; }
+
+        /// <summary>
+        /// Public Constructor for Zombie class 
+        /// </summary>
+        public Zombie()
+        {
+            _health = 0;
+        }
+        /// <summary>
+        /// New Public Constructor for Zombie where
+        /// value must be greater than or equal to 0.
+        /// If value is less than 0, throw an ArgumentException
+        /// with the message "Health must be greater or equal to 0   
+        /// </summary>
+        /// <param name="value"> value to be entered by main </param>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+            {
+                this._health = value;
+            }
+            else
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+        /// <summary>
+        /// Returns the value of health
+        /// of the Zombie object 
+        /// </summary>
+        /// <returns>private field _health</returns>
+        public int GetHealth()
+        {
+            return _health;
+        }
+
+        /// <summary>
+        /// Override ToString to print Zombie object's attributes
+        /// </summary>
+        /// <returns>Zombie objects's attributes</returns>
+        public override string ToString()
+        {
+            return $"Zombie Name: {this._name} / Total Health: {this._health}";
+        }
+    }
+}
