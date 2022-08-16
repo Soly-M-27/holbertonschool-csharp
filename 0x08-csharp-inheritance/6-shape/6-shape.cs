2ed437eae1fd2796c62bc56cc25b4b8e60b62002
Soly-M-27 <solymar227@icloud.com>
2022-08-15 23:03:11 -0400

    Shape inherits from Rectangle to set and get width and height

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Class Shape
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Method to throw new exception
+    /// </summary>
+    /// <returns> throw new NotImplementedException </returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary>
+    /// public int Width
+    /// </summary>
+    public int Width 
+    { 
+        get => width;
+        set { 
+            width = (value < 0) ? throw new ArgumentException("Width must be greater than or equal to 0") : width = value; 
+            }     
+    }
+    /// <summary>
+    /// public int Height
+    /// </summary>
+    /// <value> height </value>
+    public int Height 
+    {
+        get => height;
+        set {
+            height = (value < 0) ? throw new ArgumentException("Height must be greater than or equal to 0") : height = value;
+            }
+    }
+}
