69f9a6bdc98eca9a82503653b8a72c17172c3e9b
Soly-M-27 <solymar227@icloud.com>
2022-08-15 21:48:27 -0400

    Method that return true if subclass is derived from base class

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Public Class Obj
+/// </summary>
+public class Obj
+{
+    /// <summary>
+    /// Method that return true if derivedType is a subclass of 
+    /// baseType, otherwise returns false
+    /// </summary>
+    /// <param name="derivedType"> Derived </param>
+    /// <param name="baseType"> Base </param>
+    /// <returns> True if sublclass of Base, false if not. </returns>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+            return (true);
+        return (false);
+    }
+}
