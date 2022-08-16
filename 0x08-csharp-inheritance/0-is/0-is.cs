ca4e415234eb9baeb3e33d9c62b34e03347a16cf
Soly-M-27 <solymar227@icloud.com>
2022-08-15 20:38:33 -0400

    Added XML Documentation

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class Obj
+/// <summary>
+/// Public class Obj
+/// </summary>
+public class Obj
 {
+    /// <summary>
+    /// Method that returns True if obj is of type int
+    /// </summary>
+    /// <param name="obj"> object </param>
+    /// <returns> True if obj is of type in, False otherwise </returns>
     public static bool IsOfTypeInt(object obj)
     {
         if (obj.GetType() == typeof(int))
