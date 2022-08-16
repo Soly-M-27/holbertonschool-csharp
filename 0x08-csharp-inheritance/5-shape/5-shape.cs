40435cdb5738ee446ed0ab629bf24ce8f1ab5901
Soly-M-27 <solymar227@icloud.com>
2022-08-15 22:39:53 -0400

    Method to thorw new exception

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// Class Shape
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Method to throw new exception
+    /// </summary>
+    /// <returns> throw new NotImplementedException </returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
