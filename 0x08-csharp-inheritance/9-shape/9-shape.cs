12104ac310a2d127eeaf400092068a971c1e822d
Soly-M-27 <solymar227@icloud.com>
2022-08-15 23:46:05 -0400

    Return the area of a Square in string format

--- 0x08-csharp-inheritance/9-shape/9-shape.cs
+++ 0x08-csharp-inheritance/9-shape/9-shape.cs
@@ -1,2 +1,101 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Class Shape
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Method to throw new exception
+    /// </summary>
+    /// <returns> throw new NotImplementedException </returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary>
+    /// public int Width
+    /// </summary>
+    public int Width 
+    { 
+        get => width;
+        set { 
+            width = (value < 0) ? throw new ArgumentException("Width must be greater than or equal to 0") : width = value; 
+            }     
+    }
+    /// <summary>
+    /// public int Height
+    /// </summary>
+    /// <value> height </value>
+    public int Height 
+    {
+        get => height;
+        set {
+            height = (value < 0) ? throw new ArgumentException("Height must be greater than or equal to 0") : height = value;
+            }
+    }
+
+    /// <summary>
+    /// public override for Area methos on base class
+    /// </summary>
+    /// <returns> Area </returns>
+    public override int Area()
+    {
+        return Width * Height;
+    }
+
+    /// <summary>
+    /// public override of ToString method
+    /// </summary>
+    /// <returns> String overriden </returns>
+    public override string ToString()
+    {
+        return $"[Rectangle] {Width} / {Height}";
+    }
+
+}
+
+/// <summary>
+/// Class Square
+/// </summary>
+class Square : Rectangle
+{
+    private int size;
+    /// <summary>
+    /// public int Size property
+    /// </summary>
+    public int Size
+    {
+        get => size;
+        set
+        {
+            size = Height = Width = (value < 0) ? throw new ArgumentException("Size must be greater than or equal to 0") : size = Height = Width = value;
+        }
+    }
+
+    /// <summary>
+    /// Public override Area for size of Square
+    /// </summary>
+    /// <returns> Area of Square </returns>
+    public override int Area()
+    {
+        return size * size;
+    }
+
+    /// <summary>
+    /// public override of ToString for Square
+    /// </summary>
+    /// <returns> String of Squares Info </returns>
+    public override string ToString()
+    {
+        return $"[Square] {size} / {size}";
+    }
+}
