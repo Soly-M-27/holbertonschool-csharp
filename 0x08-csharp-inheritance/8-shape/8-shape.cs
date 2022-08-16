6e209eeb1c8604b90323c1e2f841533c72bf87ae
Soly-M-27 <solymar227@icloud.com>
2022-08-15 23:31:32 -0400

    Class Square

---
+++ 0x08-csharp-inheritance/8-shape/8-shape.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Class Shape
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Method to throw new exception
+    /// </summary>
+    /// <returns> throw new NotImplementedException </returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary>
+    /// public int Width
+    /// </summary>
+    public int Width 
+    { 
+        get => width;
+        set { 
+            width = (value < 0) ? throw new ArgumentException("Width must be greater than or equal to 0") : width = value; 
+            }     
+    }
+    /// <summary>
+    /// public int Height
+    /// </summary>
+    /// <value> height </value>
+    public int Height 
+    {
+        get => height;
+        set {
+            height = (value < 0) ? throw new ArgumentException("Height must be greater than or equal to 0") : height = value;
+            }
+    }
+
+    /// <summary>
+    /// public override for Area methos on base class
+    /// </summary>
+    /// <returns> Area </returns>
+    public override int Area()
+    {
+        return Width * Height;
+    }
+
+    /// <summary>
+    /// public override of ToString method
+    /// </summary>
+    /// <returns> String overriden </returns>
+    public override string ToString()
+    {
+        return $"[Rectangle] {Width} / {Height}";
+    }
+
+}
+
+/// <summary>
+/// Class Square
+/// </summary>
+class Square
+{
+    private int size;
+    /// <summary>
+    /// public int Size property
+    /// </summary>
+    public int Size
+    {
+        get => size;
+        set
+        {
+            size = (value < 0) ? throw new ArgumentException("Size must be greater than or equal to 0") : size = value;
+        }
+    }
+}
