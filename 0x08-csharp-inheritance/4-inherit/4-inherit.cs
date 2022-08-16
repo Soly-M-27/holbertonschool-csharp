0393ff669c29cc72d668af975fe508431d36b44f
Soly-M-27 <solymar227@icloud.com>
2022-08-15 22:33:01 -0400

    Write empty classes

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Empty Class Animal
+/// </summary>
+class Animal { }
+
+/// <summary>
+/// Empty Class Dog derived from base class Animal 
+/// </summary>
+class Dog : Animal { }
