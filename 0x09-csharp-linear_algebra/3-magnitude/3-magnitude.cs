111d8271f6120872e19ae1eddb75f4b59cf8e566
Soly-M-27 <solymar227@icloud.com>
2022-09-07 14:07:13 -0400

    send

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,25 +1,33 @@
 ﻿using System;
 
+/// <summary>
+/// Public class VectorMath
+/// </summary>
 public class VectorMath
 {
+    /// <summary>
+    /// Public static method for the Magnitude of a 2D or 3D vector
+    /// </summary>
+    /// <param name="vector"> 2D or 3D vector </param>
+    /// <returns> Rounded to the 100th double representing vector magnitude </returns>
     public static double Magnitude(double[] vector)
     {
         double res = 0f;
 
-        if (vector.GetType() != typeof(Vector2) && vector.GetType() != typeof(Vector3))
+        if (vector.GetType() !=  && vector.GetType() != typeof(Vector3))
         {
             return (-1);
         }
 
         if (vector.GetType() == typeof(Vector2))
         {
-            res = (Math.Sqrt(Math.Pow(vector.x, 2) + Math.Pow(vector.y, 2));
+            res = Math.Sqrt(Math.Pow(vector.x, 2) + Math.Pow(vector.y, 2));
         }
         else if (vector.GetType() == typeof(Vector3))
         {
-            res = (Math.Sqrt(Math.Pow(vector.x, 2) + Math.Pow(vector.y, 2) + Math.Pow(vector.z, 2));
+            res = Math.Sqrt(Math.Pow(vector.x, 2) + Math.Pow(vector.y, 2) + Math.Pow(vector.z, 2));
         }
-        
-        return (Math.Round(res/100d, 0) * 100);
+
+        return (Math.Round(res / 100d, 0) * 100);
     }
 }
--- 0x09-csharp-linear_algebra/3-magnitude/3-main.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-main.cs
@@ -1,13 +1,13 @@
 using System;
-using VectorMath;
 
 class Program
 {
     static void Main()
     {
-        double Vector vector1 = new Vector(3, 4);
-        double Vector vector2 = new Vector(3, 4, 5);
+        double vector1 = new Vector(3, 4);
+        double vector2 = new Vector(3, 4, 5);
 
-        Console.log(Magnitude(vector1));
+        Console.log(VectorMath.Magnitude(vector1));
+        Console.log(VectorMath.Magnitude(vector2));
     }
 }
