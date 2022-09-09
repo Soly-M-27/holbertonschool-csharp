e63c36442f605a4fc204627d21a57061e67c27c2
Soly-M-27 <solymar227@icloud.com>
2022-09-08 23:10:38 -0400

    attempt number 394

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -12,8 +12,8 @@ public class MatrixMath
     /// <returns> new matrix </returns>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        if (matrix1.GetLength(0) == matrix2.GetLength(0) == 
-        matrix1.GetLength(1) == matrix2.GetLength(1) == 2)
+        if (matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2 &&
+		matrix2.GetLength(0) == 2 && matrix2.GetLength(1) == 2)
         {
             double[,] res = { { 0, 0 }, { 0, 0 } };
             int mat_l = 2;
@@ -26,8 +26,8 @@ public class MatrixMath
             }
             return (res);
         }
-        else if (matrix1.GetLength(0) == matrix2.GetLength(0) == 
-        matrix1.GetLength(1) == matrix2.GetLength(1) == 3)
+        else if (matrix1.GetLength(0) == 3 && matrix1.GetLength(1) == 3 &&
+		matrix2.GetLength(0) == 3 && matrix2.GetLength(1) == 3)
         {
             double[,] res = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
             int mat_l = 3;
