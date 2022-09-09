fe0b59e46e84f17a3021c134585b36987479a95a
Soly-M-27 <solymar227@icloud.com>
2022-09-08 22:38:52 -0400

    indentation for long conditional statements

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -12,9 +12,10 @@ public class MatrixMath
     /// <returns> new matrix </returns>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        if (matrix1.GetLength(0) == matrix2.GetLength(0) == matrix1.GetLength(1) == matrix2.GetLength(1) == 2)
+        if (matrix1.GetLength(0) == matrix2.GetLength(0) == 
+            matrix1.GetLength(1) == matrix2.GetLength(1) == 2)
         {
-            double[,] res = {{0, 0}, {0, 0}}; 
+            double[,] res = { { 0, 0 }, { 0, 0 } };
             int mat_l = matrix1.Length;
             for (int x = 0; x < mat_l; x++)
             {
@@ -23,11 +24,12 @@ public class MatrixMath
                     res[x, y] = matrix1[x, y] + matrix2[x, y];
                 }
             }
-            return (res);  
+            return (res);
         }
-        else if (matrix1.GetLength(0) == matrix2.GetLength(0) == matrix1.GetLength(1) == matrix2.GetLength(1) == 3)
+        else if (matrix1.GetLength(0) == matrix2.GetLength(0) == 
+                 matrix1.GetLength(1) == matrix2.GetLength(1) == 3)
         {
-            double[,] res = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}; 
+            double[,] res = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
             int mat_l = matrix1.Length;
             for (int x = 0; x < mat_l; x++)
             {
@@ -36,8 +38,8 @@ public class MatrixMath
                     res[x, y] = matrix1[x, y] + matrix2[x, y];
                 }
             }
-            return (res);  
+            return (res);
         }
-        return new double[,] {{-1}};
+        return new double[,] { { -1 } };
     }
 }
