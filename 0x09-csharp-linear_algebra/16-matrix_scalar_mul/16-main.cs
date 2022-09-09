1ad13719bfc49f73d135a5badcc5cfb3f8d49e83
Soly-M-27 <solymar227@icloud.com>
2022-09-08 23:47:05 -0400

    matrix scalar multiplication

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-main.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-main.cs
@@ -1,13 +1,15 @@
-
 using System;
 
 class Program
 {
-    double[,] matrix2 = { { 1, 2 }, { 3, 4 } };
-    double s_2 = 4;
-    double[,] matrix3 = { { 14, -3, 0 }, { -11, -5, 3 }, { 2, -9, 13 } };
-    double s_3 = 7;
+    static void Main()
+    {
+        double[,] matrix2 = {{1, 2}, {3, 4}};
+        double s_2 = 4;
+        double[,] matrix3 = {{14, -3, 0}, {-11, -5, 3}, {2, -9, 13}};
+        double s_3 = 0.5;
 
-    Console.WriteLine("({0})", MatrixMath.MultiplyScalar(matrix2, s_2));
-    Console.WriteLine("({0})", MatrixMath.MultiplyScalar(matrix3, s_3));
+        Console.WriteLine("({0})", MatrixMath.MultiplyScalar(matrix2, s_2));
+        Console.WriteLine("({0})", MatrixMath.MultiplyScalar(matrix3, s_3));
+    }
 }
--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -1,9 +1,46 @@
 ﻿using System;
-
+/// <summary>
+/// Public Class MatrixMath with Methods MultiplyScalar
+/// </summary>
 public class MatrixMath
 {
+    /// <summary>
+    /// Public Method MultiplyScalar with multiplies a scalar by a 2D or 3D matrix
+    /// </summary>
+    /// <param name="matrix"> 2D or 3D matrix </param>
+    /// <param name="scalar"> double scalar to use in multiplication </param>
+    /// <returns> new matrix with the result from scalar multiplication </returns>
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
+        if ((matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2) || (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3))
+        {
+            if (matrix.GetLength(0) == 2)
+            {
+                double[,] res = {{0, 0}, {0, 0}};
+                int mat_l = 2;
+                for (int x = 0; x < mat_l; x++)
+                {
+                    for (int y = 0; y < mat_l; y++)
+                    {
+                        res[x, y] = matrix[x, y] * scalar;
+                    }
+                }
+                return (res);
+            }   
+            else if (matrix.GetLength(0) == 3)
+            {
+                double[,] res = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+                int mat_l = 3;
+                for (int x = 0; x < mat_l; x++)
+                {
+                    for (int y = 0; y < mat_l; y++)
+                    {
+                        res[x, y] = matrix[x, y] * scalar;
+                    }
+                }
+                return (res);
+            }   
+        }
         return new double[,] {{-1}};
     }
 }
