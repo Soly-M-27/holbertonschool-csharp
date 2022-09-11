bf599a89f666686a95e7f8ea6adbd6d76c69ff5b
Soly-M-27 <solymar227@icloud.com>
2022-09-11 19:56:51 -0400

    test

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -1,42 +1,33 @@
 ﻿using System;
-/// <summary>
-/// Public Class Matrix Math with Matrix Multiplication Method
+
+///<summary>
+/// Public Class MatrixMath
 /// </summary>
-public class MatrixMath
+class MatrixMath
 {
     /// <summary>
-    /// Public Method Multiply to multiply matrices
+    /// Public Method Multiply to multiply two matrices of different dimensions
     /// </summary>
-    /// <param name="matrix1"> multidimensional matrix </param>
-    /// <param name="matrix2"> other possible multidimensional matrix</param>
-    /// <returns> new matrix with multiplied values as result of previous matrices </returns>
-    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
-    {
-        int matrix1_row1 = matrix1.GetLength(0);
-        int matrix1_col1 = matrix1.GetLength(1);
-        int matrix2_row2 = matrix2.GetLength(0);
-        int matrix2_col2 = matrix2.GetLength(1);
+    /// <param name="matrix1"> matrix </param>
+    /// <param name="matrix2"> other matrix </param>
+    /// <returns> new matrix or -1 </returns>
+	public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+	{
+		int matrix1_row1 = matrix1.GetLength(0);
+		int matric1_col1 = matrix1.GetLength(1);
+		int matrix2_row2 = matrix2.GetLength(0);
+		int matrix2_col2 = matrix2.GetLength(1);
+        double[,] matrix3 = new double[matrix1_row1, matrix2_col2];
 
-        double[,] new_matrix = new double[matrix1_row1, matrix2_col2];
-
-        if (matrix1_col1 != matrix2_row2)
-        {
-            return new double[,] { { -1 } };
-        }
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
+            return new double[,] {{-1}};
         else
-        {
-            for (int x = 0; x < matrix1_row1; x++)
-            {
-                for (int y = 0; y < matrix2_col2; y++)
-                {
-                    for (int z = 0; z < matrix1_col1; z++)
-                    {
-                        new_matrix[x, y] = matrix1[x, z] * matrix2[z, y];
-                    }
-                }
-            }
-            return new_matrix;
-        }
-        return new double[,] {{-1}};
-    }
+		{
+			for (int i = 0; i < row1; i++)
+				for (int j = 0; j < col2; j++)
+					for (int k = 0; k < col1; k++)
+						matrix3[i, j] += matrix1[i, k] * matrix2[k, j];
+			return matrix3;
+		}
+	}
 }
