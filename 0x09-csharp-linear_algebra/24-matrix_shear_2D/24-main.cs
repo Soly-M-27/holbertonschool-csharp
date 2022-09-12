c00191b5f82fc776bc9dfc26ac1e923446950bda
Soly-M-27 <solymar227@icloud.com>
2022-09-11 21:16:26 -0400

    Shear2D Method

--- 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-main.cs
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-main.cs
@@ -4,19 +4,32 @@ class Program
 {
     static void Main()
     {
-        double[,] matrix2 = {{-1, 2}, {3, 4}};
-        double[,] matrix3 = {{1, 2, 3}, {4, 5, 6}};
+        double[,] matrix2 = {{1, 2}, {3, 4}};
         int row = matrix2.GetLength(0);
-        int col =  matrix3.GetLength(1);
+        int col =  matrix2.GetLength(1);
         double [,] new_matrix = new double[row, col];
+        char letter = 'x';
+        double shear_2D = 2;
 
-        new_matrix = MatrixMath.Multiply(matrix2, matrix3);
+        new_matrix = MatrixMath.Shear2D(matrix2, letter, shear_2D);
 
         for (int x = 0; x < new_matrix.GetLength(0); x++)
         {
             for (int y = 0; y < new_matrix.GetLength(1); y++)
             {
-                Console.WriteLine(new_matrix[x, y]);
+                Console.WriteLine(new_matrix[x, y] + ", ");
+            }
+        }
+
+        letter = 'y';
+        
+        new_matrix = MatrixMath.Shear2D(matrix2, letter, shear_2D);
+
+        for (int x = 0; x < new_matrix.GetLength(0); x++)
+        {
+            for (int y = 0; y < new_matrix.GetLength(1); y++)
+            {
+                Console.WriteLine(new_matrix[x, y] + ", ");
             }
         }
     }
--- 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -1,2 +1,43 @@
 ﻿using System;
 
+/// <summary>
+/// Public Class Matrix Math with Shear2D Method
+/// </summary>
+public class MatrixMath
+{
+    /// <summary>
+    /// Public Shear2D Method where we factor in the shear onto the matrix,
+    /// moving through the x or y axis and transform the matrix.
+    /// </summary>
+    /// <param name="matrix"> 2D matrix </param>
+    /// <param name="direction"> x or y axis direction </param>
+    /// <param name="factor"> Shear to factor in </param>
+    /// <returns> new matrix or -1 if matrix is not of correct size </returns>
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        double[,] new_matrix = new double[2, 2];
+
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+        {
+            if (direction == 'x' || direction == 'X')
+            {
+                new_matrix[0, 0] = matrix[0, 0] + matrix[0, 1] * factor;
+                new_matrix[0, 1] = matrix[0, 1];
+                new_matrix[1, 0] = matrix[1, 0] + matrix[1, 1] * factor;
+                new_matrix[1, 1] = matrix[1, 1];
+                return new_matrix;
+            }
+            else if (direction == 'y' || direction == 'Y')
+            {
+                new_matrix[0, 0] = matrix[0, 0];
+                new_matrix[0, 1] = factor * matrix[0, 0] + matrix[0, 1];
+                new_matrix[1, 0] = matrix[1, 0];
+                new_matrix[1, 1] = factor * matrix[1, 0] + matrix[1, 1]; 
+                return new_matrix;
+            }
+            else
+                return new double[,] {{-1}};
+        }   
+        return new double[,] {{-1}};
+    }
+}
