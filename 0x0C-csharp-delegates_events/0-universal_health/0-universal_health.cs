424cedc60e5cd09c64f17cd3dbb5b4ff49e37d10
Soly-M-27 <solymar227@icloud.com>
2022-11-10 17:02:50 -0400

    Adding 0-universal_health dir where C# program checks on a Player objects  maxHP

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+///<summary> Public class Player, representing an object (Player obj).</summary>
+class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    ///<summary>
+    /// Constructor prototype method for Player.
+    /// </summary>
+    ///<param name="name"> name of player</param>
+    ///<param name="maxHp">health points of player</param>
+    ///
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp > 0)
+            this.maxHp = maxHp;
+        else
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+
+    ///<summary>
+    /// Prints the current health of the player.
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
