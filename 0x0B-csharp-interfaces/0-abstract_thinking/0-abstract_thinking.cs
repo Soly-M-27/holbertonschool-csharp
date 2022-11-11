b74dbc081b65ddcb9596f072f676b09a047adff1
Soly-M-27 <solymar227@icloud.com>
2022-11-11 18:40:08 -0400

    abstract class called Base

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>
+/// Abstract Class named Base which SoftwareEnginer will inherit
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Public Property String called "name"
+    /// </summary>
+    /// <value>string</value>
+    public string name { get; set; }
+    
+    /// <summary>
+    /// Public override string ToString method to return string
+    /// </summary>
+    /// <returns>String: "{name} is a {type}"</returns>
+    public override string ToString()
+    {
+        Type type = this.GetType();
+
+        return ($"{name} is a {type}");
+    }
+}
