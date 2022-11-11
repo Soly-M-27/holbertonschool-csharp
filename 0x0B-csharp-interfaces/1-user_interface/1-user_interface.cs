fc4fb68c8042d03e2183ec801d09405fb3a0e669
Soly-M-27 <solymar227@icloud.com>
2022-11-11 19:03:44 -0400

    Adding all acreated dirs so far

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+/// <summary>
+/// Abstract Class named Base which SoftwareEnginer will inherit
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Public Property String called "name"
+    /// </summary>
+    /// <value>string</value>
+    public string name { get; set; }
+    
+    /// <summary>
+    /// Public override string ToString method to return string
+    /// </summary>
+    /// <returns>String: "{name} is a {type}"</returns>
+    public override string ToString()
+    {
+        Type type = this.GetType();
+
+        return ($"{name} is a {type}");
+    }
+}
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Interact()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Break()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Collect()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+/// <summary>
+/// Abstract Class named Base which SoftwareEnginer will inherit
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Public Property String called "name"
+    /// </summary>
+    /// <value>string</value>
+    public string name { get; set; }
+    
+    /// <summary>
+    /// Public override string ToString method to return string
+    /// </summary>
+    /// <returns>String: "{name} is a {type}"</returns>
+    public override string ToString()
+    {
+        Type type = this.GetType();
+
+        return ($"{name} is a {type}");
+    }
+}
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+class Door : Base, IInteractive
+{
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
