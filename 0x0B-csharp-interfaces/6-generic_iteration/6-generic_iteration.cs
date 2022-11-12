ea35f34cfa862c2bad17c0584af78b81b820b879
Soly-M-27 <solymar227@icloud.com>
2022-11-11 20:07:22 -0400

    Based on 5-iterate_act, remove the RoomObjects class created in the previous task. Create a new generic class Objs<T> that creates a collection of type T objects that can be iterated through using foreach.

--- 0x0B-csharp-interfaces/6-generic_iteration/6-generic_iteration.cs
+++ 0x0B-csharp-interfaces/6-generic_iteration/6-generic_iteration.cs
@@ -1,2 +1,151 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+/// <summary>
+/// Abstract Class named Base which SoftwareEnginer will inherit
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Public Property String called "name"
+    /// </summary>
+    /// <value>string</value>
+    public string name { get; set; }
+    
+    /// <summary>
+    /// Public override string ToString method to return string
+    /// </summary>
+    /// <returns>String: "{name} is a {type}"</returns>
+    public override string ToString()
+    {
+        Type type = this.GetType();
+
+        return ($"{name} is a {type}");
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class Door : Base, IInteractive
+{
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
+
+class Decoration : Base, IInteractive, IBreakable
+{
+    public int durability { get; set; }
+    public bool isQuestItem { get ;set; }
+
+    public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
+    {
+        if (durability <= 0)
+        {
+            throw new Exception("Durability must be greater than 0");
+        }
+
+        this.name = name;
+        this.durability = durability;
+        this.isQuestItem = isQuestItem;
+    }
+
+    public void Interact()
+    {
+        if (durability <= 0)
+        {
+            Console.WriteLine($"The {name} has been broken.");
+            return;
+        }
+
+        if (isQuestItem == true)
+        {
+            Console.WriteLine($"You look at the {name}. There's a key inside.");
+        }
+        else
+        {
+            Console.WriteLine($"You look at the {name}. Not much to see here.");
+        }
+    }
+
+    public void Break()
+    {
+        durability--;
+
+        if (durability > 0)
+        {
+            Console.WriteLine($"You hit the {name}. It cracks.");
+        }
+        else if (durability == 0)
+        {
+            Console.WriteLine($"You smash the {name}. What a mess.");
+        }
+        else
+        {
+            Console.WriteLine($"The {name} is already broken.");
+        }
+    }
+}
+
+class Key : Base, ICollectable
+{
+    public bool isCollected { get; set; }
+
+    public Key(string name = "Key", bool isCollected = false)
+    {
+        this.name = name;
+        this.isCollected = isCollected;
+    }
+
+    public void Collect()
+    {
+        if (isCollected == false)
+        {
+            isCollected = true;
+            Console.WriteLine($"You pick up the {name}.");
+        }
+        else
+        {
+            Console.WriteLine($"You have already picked up the {name}.");
+        }
+    }
+}
+class Objs<T> : IEnumerable<T>
+{
+    List<T> objs = new List<T>();
+
+    public void Add(T obj)
+    {
+        objs.Add(obj);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return objs.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
