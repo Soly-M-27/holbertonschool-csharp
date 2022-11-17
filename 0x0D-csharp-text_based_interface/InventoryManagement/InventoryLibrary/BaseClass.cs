c0bae3f98493eb7f229d3f0ee870b66841868724
Soly-M-27 <solymar227@icloud.com>
2022-11-16 21:54:51 -0400

    Create a new solution called InventoryManagement. In the solution, add a new .NET class library project called InventoryLibrary

---
+++ 0x0D-csharp-text_based_interface/InventoryManagement/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class BaseClass
+{
+    private string id { get; set; }
+    private DateTime date_created { get; set; }
+    private DateTime date_updated { get; set; }
+
+    public BaseClass(string id = null, DateTime date_created = default(DateTime), DateTime date_updated = default(DateTime))
+    {
+        if (id == null)
+        {
+            this.id = Guid.NewGuid().ToString();
+        }
+        else
+        {
+            this.id = id;
+        }
+
+        if (date_created == default(DateTime))
+        {
+            this.date_created = DateTime.Now;
+        }
+        else
+        {
+            this.date_created = date_created;
+        }
+
+        if (date_updated == default(DateTime))
+        {
+            this.date_updated = DateTime.Now;
+        }
+        else
+        {
+            this.date_updated = date_updated;
+        }
+    }
+
+}
